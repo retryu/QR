757c245c4da384eac1686594e1998ae5f0064646
retryu <ruanchenyugood@gmail.com>
2012-09-20 16:06:53 +0800

    init

--- src/com/model/User.java
+++ src/com/model/User.java
@@ -1,5 +1,41 @@
 package com.model;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
 public class User {
-	private String  name;
+
+	private  String id;
+	private  String  passWord;
+	private  String  e_mail;
+	
+	
+	
+	public String getId() {
+		return id;
+	}
+	@Id
+	@GeneratedValue
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public String getE_mail() {
+		return e_mail;
+	}
+	public void setE_mail(String e_mail) {
+		this.e_mail = e_mail;
+	}
+	
+	
+	
 }
